75f7898f5489e37e224da06f2da6f94a94b8c3ae
xardo <pardomoyasegio@gmail.com>
2026-03-25 18:35:17 -0500

    fetch: corrección clase estatica

--- src/WebApi/Controllers/OrdersController.cs
+++ src/WebApi/Controllers/OrdersController.cs
@@ -3,7 +3,7 @@ using System;
 // BAD: Mixing minimal APIs with Controllers folder just to confuse structure
 namespace WebApi.Controllers
 {
-    public class OrdersController /* No ControllerBase, no attributes: unused on purpose */ 
+    public static class OrdersController /* No ControllerBase, no attributes: unused on purpose */ 
     {
         public static string DoNothing() => "This controller does nothing. Endpoints are in Program.cs";
     }
